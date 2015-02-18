58a86a7dc3eb48cb996dfc20e67c6638ad3437ec
Michael Parker <parker_michael@me.com>
2015-02-18 15:26:12 +1100

    migrated to gulp

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp');
+var bower = require ('gulp-bower');
+var sass = require ('gulp-sass');
+var notify = require ('gulp-notify');
+// var templateCache = require ('gulp-angular-templatecache')
+var browserSync = require ('browser-sync');
+// var modRewrite  = require('connect-modrewrite');
+// var middleware = require('middleware');
+// var streamqueue = require('streamqueue');
+
+//Compile views into an angular $templateCache module
+//Move them to a temp folder, we'll add them to public later
+
+
+//Move files in html to public/
+gulp.task('html', function(){
+	gulp.src('./build/html/*.html')
+		.pipe(gulp.dest('./public/'));
+});
+
+
+//move both the templatecache service and angular app to public/js
+gulp.task('scripts', ['html'], function(){
+	gulp.src('./build/js/*.js')
+		.pipe(gulp.dest('./public/js/'));
+});
+
+//compile SASS and then move it to public/css
+//Notify when build is complete
+gulp.task('build', ['scripts'], function(){
+	gulp.src('./build/scss/*scss')
+		.pipe(sass({
+	        style: 'compressed',
+	        errLogToConsole: false,
+	        onError: function(err) {
+	            return notify().write(err);
+	        }
+	    }))
+		.pipe(gulp.dest('./public/css/'))
+		.pipe(notify("Build - Success!"));
+});
+
+//move bower components to the library folder
+gulp.task('bower', function(){
+	return bower()
+		.pipe(gulp.dest('./public/js/lib/'));
+});
+
+//watch these files and run the build if they update
+gulp.task('watch', function(){
+    gulp.watch(
+        ['./build/html/*.html',
+        './build/js/*.js',
+        './build/scss/**/*.scss',
+        './bower_components'],
+        ['build']
+    )
+});
+
+//Create a local web server using browser-sync
+//Refresh the browser if any files change
+gulp.task('serve', function () {
+		var files = [
+		'./public/**/*.html',
+		'./public/src/**/*.js',
+		'./public/src/css/*.css',
+	];
+
+    browserSync.init(files, {
+        server: {
+            baseDir: 'public/',
+        }
+    });
+});
+
+//default task
+gulp.task('default', ['build', 'bower', 'watch', 'serve']);
+
