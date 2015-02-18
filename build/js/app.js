8e046ced0c12c90602bd362a72747670c56d2e4f
Michael Parker <parker_michael@me.com>
2015-02-18 17:38:17 +1100

    Started styles

--- build/js/app.js
+++ build/js/app.js
@@ -3,8 +3,11 @@ var console = {log: function() {}};
 var app = angular.module('app',[]);
 
 app.controller('main-controller', function($scope, $http, $interval){
+
+	//WEATHER
 	$http.get('http://api.openweathermap.org/data/2.5/weather?id=2147714&APPID=2eca592cb736665e4889c10c8374cb9b')
 		.success(function(data){
+			$scope.rawWeather = data;
 			$scope.weather = {
 				"currentTemp" : Math.round(data.main.temp*0.1),
 				"maximumTemp" : Math.round(data.main.temp_max*0.1),
@@ -17,14 +20,31 @@ app.controller('main-controller', function($scope, $http, $interval){
 			console.log("GET weather failed with code: "+status);
 		});
 
-		$interval(function(){
-			var date = new Date();
-			var s = date.getSeconds();
-			var m = date.getMinutes();
-			var h = date.getHours();
-			$scope.time = h +" "+m+" "+s;
-		}, 1000);
-	
+	//CLOCK
+	$interval(function(){
+		var addZero = function(number){
+			if(number < 10){
+				return "0"+number;
+			}
+			else {
+				return number;
+			}
+		};
+		var date = new Date();
+		var s = addZero(date.getSeconds());
+		var m = addZero(date.getMinutes());
+		var h = addZero(date.getHours());
+		$scope.time = h+":"+m+":"+s;
+	}, 1000);
+
+	//HEADLINES
+	$http.get('http://content.guardianapis.com/australia-news?api-key=fmhmn6m4jy8g5hcgr2rsm622')
+		.success(function(data){
+			$scope.news = data.response;
+		})
+		.error(function(status){
+			console.log("error getting news: " +status)
+		});
 });
 
 
